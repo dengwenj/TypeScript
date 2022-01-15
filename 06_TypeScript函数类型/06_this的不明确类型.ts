2e4d8a45cca3bf30261d5a4748548e947f31f190
dengwenj <927561237@qq.com>
2022-01-16 00:08:16 +0800

    this的不明确类型

--- 06_TypeScript函数类型/06_this的不明确类型.ts
+++ 06_TypeScript函数类型/06_this的不明确类型.ts
@@ -8,27 +8,39 @@
 // }
 // obj.foo()
 
+// 声明 this 的类型，可以使用哪些数据就写在这里
 type ThisType = { name: string, age: number };
 
 // 第一个中写的 this
 function eating(this: ThisType, message: string) {
-  console.log(this)
-  console.log(this.age)
+  console.log(this) // { name: 'dwj', age: 21, sex: '男', eating: [Function: eating] }
+  console.log(this.age) // 21
   console.log(this.name + message);
-  // console.log(this.name + " eating", message);
+  // console.log(this.sex); // 类型“ThisType”上不存在属性“sex”。
+  
 }
 
 const info = {
   name: "dwj",
   age: 21,
+  sex: '男',
   eating: eating,
 };
 
 // 隐式绑定
-info.eating("哈哈哈");
+// info.eating("哈哈哈");
 
 // 显示绑定
 // eating.call({name: "kobe"}, "呵呵呵")
 // eating.apply({name: "james"}, ["嘿嘿嘿"])
 
 export {};
+
+
+type ThisType1 = { name: string, saying: () => void }
+function foo3(this: ThisType1, message: string) {
+  console.log(this) // { name: 'zww', saying: [Function: saying], sex: '女' }
+  this.saying()
+  console.log(this.name)
+}
+foo3.call({ name: 'zww', saying() {console.log('dwj')}, sex: '女' }, 'dd')
