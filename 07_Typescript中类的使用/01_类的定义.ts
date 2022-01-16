8d7333cf323369295c03a8054b790ddc2988ad4f
dengwenj <927561237@qq.com>
2022-01-16 15:10:27 +0800

    类的定义

---
+++ 07_Typescript中类的使用/01_类的定义.ts
@@ -0,0 +1,17 @@
+class Person {
+  // ts 中把很多动态的都删除掉，不让随便加
+  name: string // 必须要写
+  age: number
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+
+  say() {
+    console.log('say')
+  }
+}
+
+const p = new Person('dengwenj', 21)
+console.log(p);
+p.say()
